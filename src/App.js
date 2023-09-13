7bf19fe26f3e946336638338552504e62e09916d
childevnote <daniel011125@gmail.com>
2023-09-13 18:47:29 +0900

    1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
+import React, { useState } from "react";
 import './App.css';
 
 function App() {
+
+  const [budgetData, setBudgetData] = useState([]);
+  const [value, setValue] = useState("");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>예산 계산기</h1>
+      <div className="input-info">
+        <div className="pay">
+          <p>지출 항목</p>
+          <input
+            type="text"
+          />
+        </div>
+        <div className="cost">
+          <p>비용</p>
+          <input
+            type="number"
+          />
+          <input className="submitBtn" type="submit" value="추가" />
+        </div>
+
+      </div>
     </div>
   );
 }
